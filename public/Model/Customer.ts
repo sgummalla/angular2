26c591b3985bae5ea297aae7112dcd62c41abab2
Suman Gummmalla <gvnpsuman@live.com>
2016-11-29 11:50:25 -0500

    Lab 03 Test

--- public/Model/Customer.ts
+++ public/Model/Customer.ts
@@ -2,5 +2,4 @@ export class Customer {
     public CustomerName: string = "";
     public CustomerCode: string = "";
     public CustomerAmount: number = 0;
-    public CustomerAmount1: number = 0;
 }
